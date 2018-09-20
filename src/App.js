d93a6e68c4a4224e86fcf8421026d18553546fb8
Chirayut <keemsignificant@gmail.com>
2018-09-21 01:29:57 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className = "card">
-        {555555
+        {1234
           Array.from(word).map((c,i)=><CharacterCard value={c} key={i}/>)
         }
         
